3b25bfea562a5bb1be8c27e591d4b190c97f67d9
diop <romain.diop@amundi.com>
2023-06-25 17:31:36 +0200

    V3

--- script.sh
+++ script.sh
@@ -1,11 +1,16 @@
 #!/bin/bash
 
+# Clean output file if it exists
+if [[ -f output.txt ]]; then
+	rm output.txt
+fi
+
 for file in *; do
 	if [[ -f $file ]]; then
-		echo "Title: $file"
-		echo "Content:"
-		cat "$file"
-		echo "------------------------"
+		echo "Title: $file" >> output.txt
+		echo "Content:" >> output.txt
+		cat "$file" >> output.txt
+		echo "------------------------" >> output.txt
 	fi
 done
 
